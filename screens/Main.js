307f26c35698c176462e74f029830257bae15af3
unknown <vasilis.litsas@architechts.gr>
2018-10-21 21:31:51 +0300

    added sign out button

--- screens/Main.js
+++ screens/Main.js
@@ -1,23 +1,41 @@
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
 import firebase from 'react-native-firebase'
 
 export default class Main extends React.Component {
 
-  state = { currentUser: null }
+  state = {
+    currentUser: null,
+    errorMessage: ''
+  }
 
   componentDidMount() {
     const { currentUser } = firebase.auth()
     this.setState({ currentUser })
   }
 
+  signout = () => {
+    firebase.auth().signOut().then(function () {
+      // Sign-out successful.
+      this.props.navigation.navigate('Login')
+    }).catch(function (error) {
+      // An error happened.
+      // this.setState({ errorMessage: error.message })
+    });
+  }
+
   render() {
     const { currentUser } = this.state
     return (
       <View style={styles.container}>
+        <Text style={{ color: 'red' }} >
+          {this.state.errorMessage}
+        </Text>
         <Text>
           Hi {currentUser && currentUser.email}!
         </Text>
+
+        <Button title="Sign out" onPress={this.signout} />
       </View>
     )
   }
